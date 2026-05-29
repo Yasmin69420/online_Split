55444bd1dc8caaa49abff7fd9461576802b7fa45
Menukaw <s4251547@student.rmit.edu.au>
2026-05-29 13:30:49 +1000

    Navigation.java
    2nd ver, made it so that directional inputs are binded to wasd, and added every navigable room

--- Navigation.java
+++ Navigation.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 public class Navigation {
     public static Scanner myInput = new Scanner(System.in);
-    private static int currentLocation = 220;
+    private static int currentLocation = 110;
     public static int tempLocation;
     private static boolean gameRunning = true;
 
@@ -13,11 +13,17 @@ public class Navigation {
     }
     public static void gotoLocation(int locationEntered){
         switch (locationEntered){
+            case 110:
+                room110();
+                break;
             case 120:
                 room120();
                 break;
             case 130:
                 room130();
+                break;
+            case 140:
+                room140();
                 break;
             case 210:
                 room210();
@@ -25,8 +31,35 @@ public class Navigation {
             case 220:
                 room220();
                 break;
+            case 230:
+                room230();
+                break;
+            case 240:
+                room240();
+                break;
+            case 310:
+                room310();
+                break;
             case 320:
                 room320();
+                break;
+            case 330:
+                room330();
+                break;
+            case 340:
+                room340();
+                break;
+            case 410:
+                room410();
+                break;
+            case 420:
+                room420();
+                break;
+            case 430:
+                room430();
+                break;
+            case 440:
+                room440();
                 break;
             default:
                 System.out.println("You can't go that way!");
@@ -34,11 +67,17 @@ public class Navigation {
         }
 
     }
+    public static void room110() {
+        System.out.println("Welcome to room 110");
+    }
     public static void room120(){
         System.out.println("Welcome to room 120");
     }
     public static void room130(){
         System.out.println("Welcome to room 130");
+    }
+    public static void room140() {
+        System.out.println("Welcome to room 140");
     }
     public static void room210(){
         System.out.println("Welcome to room 210");
@@ -46,33 +85,60 @@ public class Navigation {
     public static void room220(){
         System.out.println("Welcome to room 220");
     }
+    public static void room230() {
+        System.out.println("Welcome to room 230");
+    }
+    public static void room240() {
+        System.out.println("Welcome to room 240");
+    }
+    public static void room310() {
+        System.out.println("Welcome to room 310");
+    }
     public static void room320(){
         System.out.println("Welcome to room 320");
+    }
+    public static void room330() {
+        System.out.println("Welcome to room 330");
+    }
+    public static void room340() {
+        System.out.println("Welcome to room 340");
+    }
+    public static void room410() {
+        System.out.println("Welcome to room 410");
+    }
+    public static void room420() {
+        System.out.println("Welcome to room 420");
+    }
+    public static void room430() {
+        System.out.println("Welcome to room 430");
+    }
+    public static void room440() {
+        System.out.println("Welcome to room 440");
     }
     public static void navigate(){
         tempLocation = currentLocation;
 
         System.out.println("You currently in section " + currentLocation);
         System.out.println("which direction would you like to go?");
-        System.out.println("[n]orth, [s]outh, [e]ast, [w]est\nPress [q] to quit");
-        System.out.println("Enter Direction n,s,e,w");
+        System.out.println("North [w], South [s], East [d], West [a]\nPress [q] to quit");
+        System.out.println("Enter Direction w,s,d,a");
         String directionEntered = myInput.nextLine().toLowerCase();
 
         switch (directionEntered){
 
-            case "n":
+            case "s":
                 currentLocation = currentLocation + 100;
                 gotoLocation(currentLocation);
                 break;
-            case "s":
+            case "w":
                 currentLocation = currentLocation - 100;
                 gotoLocation(currentLocation);
                 break;
-            case "w":
+            case "a":
                 currentLocation = currentLocation - 10;
                 gotoLocation(currentLocation);
                 break;
-            case "e":
+            case "d":
                 currentLocation = currentLocation + 10;
                 gotoLocation(currentLocation);
                 break;
@@ -83,3 +149,4 @@ public class Navigation {
         }
     }
 }
+
